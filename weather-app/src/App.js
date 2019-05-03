72e50e1d4753a48a9757a483fc059d52ad2d851f
shashi561 <shashishekhar561@gmail.com>
2019-05-03 09:52:27 +0530

    final commit

---
+++ weather-app/src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import './App.css';
+import Title from "./Component/title";
+import Form from "./Component/form";
+import Weather from "./Component/weather";
+
+const API_KEY = "3841b2763b94d08adf0efddcedbba16c";
+
+class App extends Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+    const data = await api_call.json();
+    console.log(data);
+    if (city && country) {
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ''
+      })
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter the Values."
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Title />
+        <Form getWeather={this.getWeather} />
+        <Weather
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+        />
+      </div>
+    );
+  }
+}
+export default App;
---
+++ weather-app/src/Component/title.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Title = () => (
+    <div>
+        <h2>Weather Finder</h2>
+        <h3>Find out Temperature, Conditions and more...</h3>
+    </div>
+);
+
+export default Title;
